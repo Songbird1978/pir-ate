104eb43111ef1b7039a6c9185bfae867a4320394
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-05-02 12:44:24 +0100

    fixed errors and added box-shadow to articles - json file now working

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
+//import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import data from '../../Data.json';
 import { Link } from 'react-router-dom';
@@ -16,25 +16,36 @@ function Article() {
 
   return (
 
-    <Grid container className="articleGrid">
+    <Grid container className="articleGrid" spacing={6}>
+      <div>
+        <title className="pageTitle"> Articles</title>
+      </div>
 
       {data.map((article) => {
         return (
 
-          <Grid item xs={12} className="GridItem" key={article.id} >
+          <Grid item xs={article.xs} sm={article.sm} className="GridItem" key={article.id} >
 
-            <Card className={article.className}>
-              <CardMedia component={article.icon} className={article.className} key={article.image} alt={article.imageName} style={{ scale: '1', padding: "0", left: "0", right: "0" }}></CardMedia>
+            <Card id="card" className={article.className}>
 
               <CardContent>
+              <div className="imageDiv">
+                <img src={article.image} className={article.className} key={article.image} alt={article.imageName} style={{ scale: '1', padding: "0", left: "0", right: "0" }}></img>
+                </div>
+                <div className="titleDiv">
                 <h1 className="articleTitle" >{article.title}</h1>
+                </div>
+                <div className="infoDiv">
                 <p className="articleDescription" >{article.description}</p>
+                </div>
+               
               </CardContent>
               <CardActions>
-                <Button size="large">
-                  <Link to={article.link} className={article.className}>
-                  </Link>
-                </Button>
+                <Link to={article.link} className={article.className}>
+                  <button className="cardBtn" size="large">
+                    Read More
+                  </button>
+                </Link>
               </CardActions>
             </Card>
 
