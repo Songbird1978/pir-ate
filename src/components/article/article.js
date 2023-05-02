d3cf3a4b1fe315090412eb2aca1596528c5092d6
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-05-02 09:12:21 +0100

    added articles page but now images links are not working

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-//import CardMedia from '@mui/material/CardMedia';
-//import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Data from '../../Data.json';
 import { Link } from 'react-router-dom';
 import "../../index.css";
 import "../article/article.css";
+
 
 
 function Article() {
@@ -18,14 +21,21 @@ function Article() {
       {Data.map((Article) => {
         return (
 
-          <Grid item xs={Article.xs} className="GridItem" key={Article.id}>
+          <Grid item xs={12} className="GridItem" key={Article.id} >
 
             <Card className={Article.className}>
-              <Link to={Article.link} className={Article.className}>
-                <img src={Article.imageUrl} className={Article.className} key={Article.imageUrl} alt={Article.imageName} style={{scale: '1', padding: "0", left: "0", right: "0"}}></img>
+              <CardMedia component={Article.imageUrl} className={Article.className} key={Article.imageUrl} alt={Article.imageName} style={{ scale: '1', padding: "0", left: "0", right: "0" }}></CardMedia>
+
+              <CardContent>
                 <h1 className="articleTitle" >{Article.title}</h1>
                 <p className="articleDescription" >{Article.description}</p>
-              </Link>
+              </CardContent>
+              <CardActions>
+                <Button size="large">
+                  <Link to={Article.link} className={Article.className}>
+                  </Link>
+                </Button>
+              </CardActions>
             </Card>
 
           </Grid>
